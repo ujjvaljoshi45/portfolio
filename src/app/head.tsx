fa713a193f68a0e847a1c6ad551020d944180017
ujjval <ujjvaljoshi45@gmail.com>
2025-07-26 18:15:59 +0530

    head fix

---
+++ src/app/head.tsx
@@ -0,0 +1,27 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="-Aioto8fyXjo1TgS1CHRphFWle6UWJgseeM56yVXqQA"
+      />
+
+      <title>Ujjval Joshi | Portfolio</title>
+      <meta
+        name="description"
+        content="Portfolio website of Ujjval Joshi. Explore my work, experience, education, and skills."
+      />
+      <meta name="author" content="Ujjval Joshi" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <meta property="og:title" content="Ujjval Joshi | Portfolio" />
+      <meta
+        property="og:description"
+        content="Explore the portfolio of Ujjval Joshi: projects, experience, education, and skills."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://ujjvaljoshi.is-a.dev" />
+      <meta name="linkedin" content="www.linkedin.com/in/ujjvaljoshi45" />
+    </>
+  );
+}
